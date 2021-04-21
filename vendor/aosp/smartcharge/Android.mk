syberia-project/platform_hardware_syberia

Initial HIDL based simple SmartCharge implementation
Based on LOS HIDL

Only for triggering sysfs node for now

Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: I2f876c6060a42816c02d3a7c48ab57b02f39c751

smartcharge/Android.mk
@@ -1,0 +1,43 @@
+#
+# Copyright (C) 2021 Syberia Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    SmartCharge.cpp \
+    service.cpp
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/include \
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libbinder \
+    libhidlbase \
+    libhidltransport \
+    libutils \
+    vendor.syberia.smartcharge@1.0
+
+LOCAL_MODULE := vendor.syberia.smartcharge@1.0-service
+LOCAL_INIT_RC := vendor.syberia.smartcharge@1.0-service.rc
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := syberia
+LOCAL_VINTF_FRAGMENTS := vendor.syberia.smartcharge@1.0-service.xml
+
+include $(BUILD_EXECUTABLE)

smartcharge/SmartCharge.cpp
@@ -1,0 +1,62 @@
+/*
+ * Copyright (C) 2021 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "smartcharge@1.0-service"
+
+#include "SmartCharge.h"
+#include <android-base/logging.h>
+#include <fstream>
+#include <iostream>
+#include "smartcharge.h"
+
+namespace vendor {
+namespace syberia {
+namespace smartcharge {
+namespace V1_0 {
+namespace implementation {
+
+/*
+ * Write value to path and close file.
+ */
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+
+    if (!file) {
+        PLOG(ERROR) << "Failed to open: " << path;
+        return;
+    }
+
+    LOG(DEBUG) << "write: " << path << " value: " << value;
+
+    file << value << std::endl;
+
+    if (!file) {
+        PLOG(ERROR) << "Failed to write: " << path << " value: " << value;
+    }
+}
+
+Return<bool> SmartCharge::setChargingEnabled(bool enable) {
+    set(CHARGE_CONTROL_PATH, enable ? 0 : 1);
+
+    return enable;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace smartcharge
+}  // namespace syberia
+}  // namespace vendor

smartcharge/SmartCharge.h
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2021 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_SYBERIA_SMARTCHARGE_V1_0_SMARTCHARGE_H
+#define VENDOR_SYBERIA_SMARTCHARGE_V1_0_SMARTCHARGE_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/syberia/smartcharge/1.0/ISmartCharge.h>
+
+namespace vendor {
+namespace syberia {
+namespace smartcharge {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::sp;
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+
+using ::vendor::syberia::smartcharge::V1_0::ISmartCharge;
+
+
+struct SmartCharge : public ISmartCharge {
+    Return<bool> setChargingEnabled(bool enable) override;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace smartcharge
+}  // namespace syberia
+}  // namespace vendor
+
+#endif  // VENDOR_SYBERIA_SMARCHARGE_V1_0_SMARTCHARGE_H

smartcharge/service.cpp
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2021 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "fastcharge@1.0-service"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "SmartCharge.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
+using vendor::syberia::smartcharge::V1_0::ISmartCharge;
+using vendor::syberia::smartcharge::V1_0::implementation::SmartCharge;
+
+using android::OK;
+using android::status_t;
+
+int main() {
+    android::sp<SmartCharge> service = new SmartCharge();
+
+    configureRpcThreadpool(1, true);
+
+    status_t status = service->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Cannot register SmartCharge HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "SmartCharge HAL service ready.";
+
+    joinRpcThreadpool();
+
+    LOG(ERROR) << "SmartCharge HAL service failed to join thread pool.";
+    return 1;
+}

AospExtended/platform_hardware_aosp

vendor/aosp/smartcharge/Android.mk
@@ -1,0 +1,42 @@
+#
+# Copyright (C) 2021 Syberia Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    SmartCharge.cpp \
+    service.cpp
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/include \
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libbinder \
+    libhidlbase \
+    libutils \
+    vendor.aosp.smartcharge@1.0
+
+LOCAL_MODULE := vendor.aosp.smartcharge@1.0-service
+LOCAL_INIT_RC := vendor.aosp.smartcharge@1.0-service.rc
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := aosp
+LOCAL_VINTF_FRAGMENTS := vendor.aosp.smartcharge@1.0-service.xml
+
+include $(BUILD_EXECUTABLE)

vendor/aosp/smartcharge/SmartCharge.cpp
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2021 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "smartcharge@1.0-service"
+
+#include "SmartCharge.h"
+#include <android-base/logging.h>
+#include <fstream>
+#include <iostream>
+#include "smartcharge.h"
+
+namespace vendor {
+namespace aosp {
+namespace smartcharge {
+namespace V1_0 {
+namespace implementation {
+
+/*
+ * Write value to path and close file.
+ */
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+
+    if (!file) {
+        PLOG(ERROR) << "Failed to open: " << path;
+        return;
+    }
+
+    LOG(DEBUG) << "write: " << path << " value: " << value;
+
+    file << value << std::endl;
+
+    if (!file) {
+        PLOG(ERROR) << "Failed to write: " << path << " value: " << value;
+    }
+}
+
+Return<bool> SmartCharge::setChargingEnabled(bool enable) {
+    set(CHARGE_CONTROL_PATH, enable ? 1 : 0);
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace smartcharge
+}  // namespace aosp
+}  // namespace vendor

vendor/aosp/smartcharge/SmartCharge.h
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2021 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_aosp_SMARTCHARGE_V1_0_SMARTCHARGE_H
+#define VENDOR_aosp_SMARTCHARGE_V1_0_SMARTCHARGE_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/aosp/smartcharge/1.0/ISmartCharge.h>
+
+namespace vendor {
+namespace aosp {
+namespace smartcharge {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::sp;
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+
+using ::vendor::aosp::smartcharge::V1_0::ISmartCharge;
+
+
+struct SmartCharge : public ISmartCharge {
+    Return<bool> setChargingEnabled(bool enable) override;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace smartcharge
+}  // namespace aosp
+}  // namespace vendor
+
+#endif  // VENDOR_AOSP_SMARCHARGE_V1_0_SMARTCHARGE_H

vendor/aosp/smartcharge/service.cpp
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2021 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "smartcharge@1.0-service"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "SmartCharge.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
+using vendor::aosp::smartcharge::V1_0::ISmartCharge;
+using vendor::aosp::smartcharge::V1_0::implementation::SmartCharge;
+
+using android::OK;
+using android::status_t;
+
+int main() {
+    android::sp<SmartCharge> service = new SmartCharge();
+
+    configureRpcThreadpool(1, true);
+
+    status_t status = service->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Cannot register SmartCharge HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "SmartCharge HAL service ready.";
+
+    joinRpcThreadpool();
+
+    LOG(ERROR) << "SmartCharge HAL service failed to join thread pool.";
+    return 1;
+}